1268f1cc25c4660933144222b28622754ef4aed6
Syed Ahkam <smahkam57@gmail.com>
2024-11-18 01:22:02 +0530

    fix: remove unnecessary pkgs from shell.nix

--- shell.nix
+++ shell.nix
@@ -17,12 +17,6 @@ mkShell.override
       llvmPackages.libclang
       pipewire
       clang-tools
-      gst_all_1.gstreamer
-      gst_all_1.gstreamer.dev
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
     ];
     LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
   }
