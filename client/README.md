d0f0af3d5d506c38197871dd2e3b47044bb4c075
Syed Ahkam <smahkam57@gmail.com>
2024-11-22 21:08:29 +0530

    feat: init the client app

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# way2adb_client
+
+This project is a template for an Android Application. It is intended to be used with [android-cli](https://github.com/SyedAhkam/android-cli).
---
+++ client/shell.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {
+  config = {
+    android_sdk.accept_license = true;
+    allowUnfree = true;
+  };
+};
+
+let
+  buildToolsVersion = "33.0.3";
+  androidComposition = androidenv.composeAndroidPackages {
+    buildToolsVersions = [
+      buildToolsVersion
+    ];
+    platformVersions = [ "33" ];
+    abiVersions = [
+      "x86_64"
+    ];
+  };
+in
+mkShell rec {
+  ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
+
+  buildInputs = [
+    androidComposition.androidsdk
+    jdk17
+  ];
+}
